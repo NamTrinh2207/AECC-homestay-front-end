1310468147e0b7d3c0cc97cd7f575fbab90c73aa
NamTrinh2207 <trinhnam180697@gmail.com>
2023-05-23 00:44:55 +0700

    fix uplaod avatar user

--- src/components/Login/FormChangePassword.js
+++ src/components/Login/FormChangePassword.js
@@ -2,18 +2,27 @@ import React from 'react';
 import axios from "axios";
 import {toast, ToastContainer} from "react-toastify";
 import {Formik} from "formik";
+import * as Yup from "yup";
 
 function FormChangePassword(props) {
     const user = props.user;
     const initialValue = {
         username: user.username || "",
-        password: user.password || "",
-        confirmPassword:  "",
+        password: "",
+        confirmPassword: ""
     };
+    const validationSchema = Yup.object().shape({
+        password: Yup.string()
+            .required('Vui lòng nhập mật khẩu mới')
+            .min(6, 'Mật khẩu mới phải có ít nhất 6 ký tự'),
+        confirmPassword: Yup.string()
+            .oneOf([Yup.ref('password'), null], 'Mật khẩu xác nhận phải khớp với mật khẩu mới')
+            .required('Vui lòng xác nhận mật khẩu mới'),
+    });
     return (
         <Formik initialValues={initialValue}
                 onSubmit={(values) => {
-                    axios.put(`http://localhost:8080/change${user.id}`, values)
+                    axios.put(`http://localhost:8080/change/${user.id}`, values)
                         .then(() => {
                             toast.success("Đổi mật khẩu tin thành công")
                         })
@@ -34,9 +43,23 @@ function FormChangePassword(props) {
                         </div>
                         <div className="col-lg-12 ">
                             <div className="form-group number">
-                                <label>Tên tài khoản</label>
-                                <input type="text" {...formik.getFieldProps("password")} className="form-control"
-                                       placeholder="Mật khẩu"/>
+                                <label>Nhập mật khẩu cũ</label>
+                                <input type="password" {...formik.getFieldProps("password")} className="form-control"
+                                       placeholder="Mật khẩu cũ"/>
+                            </div>
+                        </div>
+                        <div className="col-lg-12 ">
+                            <div className="form-group number">
+                                <label>Mật khẩu mới</label>
+                                <input type="password" {...formik.getFieldProps("confirmPassword")} className="form-control"
+                                       placeholder="Mật khẩu mới"/>
+                            </div>
+                        </div>
+                        <div className="col-lg-12 ">
+                            <div className="form-group number">
+                                <label>Nhập lại mật khẩu mới</label>
+                                <input type="password" {...formik.getFieldProps("confirmPassword")} className="form-control"
+                                       placeholder="Nhập lại mật khẩu mới"/>
                             </div>
                         </div>
                         <div className="col-lg-12">
