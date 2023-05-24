18e4804349acc02a80b293d568023da87fb0a37a
huongly9596789 <huongly95967@gmail.com>
2023-05-24 20:08:10 +0700

    pull dev show detail

--- src/components/Login/MyProperty.jsx
+++ src/components/Login/MyProperty.jsx
@@ -5,6 +5,8 @@ function MyProperty(props) {
     const userId = props.user;
     const [user, setUser] = useState(null);
     const [homes, setHomes] = useState([]);
+    const [selectedStatus, setSelectedStatus] = useState(null);
+
 
     useEffect(() => {
         const fetchUserData = async () => {
@@ -25,6 +27,33 @@ function MyProperty(props) {
     if (!user) {
         return <div>Loading...</div>;
     }
+    const updateUser = async (newUserData,id) => {
+        try {
+            const response = await axios.put(`http://localhost:8080/homes/${id}`, newUserData);
+            // Xử lý kết quả trả về từ server (nếu cần)
+            console.log(response.data);
+            console.log(homes.id)
+        } catch (error) {
+            // Xử lý lỗi (nếu cần)
+            console.log(error);
+        }
+    };
+
+    const handleChangeStatus = async (event, homeId) => {
+        const newStatus = event.target.value;
+        const updatedHomes = homes.map(home => {
+            if (home.id === homeId) {
+                return { ...home, status: newStatus };
+            }
+            return home;
+        });
+        setHomes(updatedHomes);
+        setSelectedStatus(newStatus);
+
+        // Gửi yêu cầu cập nhật trạng thái cho homeId lên server
+        const newUserData = { homes: updatedHomes };
+        await updateUser(newUserData,homeId);
+    };
 
     return (
         homes.length > 0 ? (
@@ -54,11 +83,20 @@ function MyProperty(props) {
                                             <a href="#"><i className="fa fa-pencil"></i> Edit</a>
                                         </li>
                                         <li>
-                                            <a href="#"><i className="fa  fa-eye-slash"></i> Hide</a>
-                                        </li>
-                                        <li>
                                             <a href="#" className="delete"><i
                                                 className="fa fa-remove"></i> Delete</a>
+                                        </li>
+                                        <li>
+                                            <select
+                                                id={`status-${home.id}`}
+                                                className="fa"
+                                                value={home.status}
+                                                onChange={(event) => handleChangeStatus(event, home.id)}
+                                            >
+                                                <option value="1">Phòng Trống</option>
+                                                <option value="2">Đang bảo trì</option>
+                                                <option value="3">Đang cho thuê</option>
+                                            </select>
                                         </li>
                                     </ul>
                                 </td>
