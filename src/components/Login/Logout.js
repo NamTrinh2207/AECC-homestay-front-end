92d2305dc31b2536ccb58ccb2ff60818c34438fd
NamTrinh2207 <trinhnam180697@gmail.com>
2023-05-30 08:20:54 +0700

    .

--- src/components/Login/Logout.js
+++ src/components/Login/Logout.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import HomePage from "../HomePage";
+import Swal from 'sweetalert2';
 
 const Logout = () => {
+
     localStorage.removeItem('user');
     return <HomePage />
 };
