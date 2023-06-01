cf5c47af62b697ae869ecb22a81299acd43f9f1f
luc-nguyen-174 <louiskingvn@gmail.com>
2023-06-01 10:10:56 +0700

    delete set avatar

--- src/components/header/TopHeader.jsx
+++ src/components/header/TopHeader.jsx
@@ -21,9 +21,6 @@ function TopHeader(props) {
         });
     };
     const data = localStorage.getItem("user");
-    const defaultAvatar = "https://firebasestorage.googleapis.com/v0/b/react-demo-d28f4.appspot.com/o/logo%2Favatar-13.jpg?alt=media&token=bfda6ea1-cd69-4680-92e5-9e4dcb720159";
-
-    let [avatar, setAvatar] = useState("");
 
     let user = null;
     if (data != null) {
@@ -31,17 +28,6 @@ function TopHeader(props) {
     } else {
         user = null;
     }
-
-    useEffect(() => {
-        if (user !=null){
-            if (user.avatar != null) {
-                setAvatar(user.avatar);
-            } else {
-                setAvatar(defaultAvatar);
-            }
-        }
-    });
-
     return (
         <div>
             <header className="top-header th-bg" id="top-header-2">
@@ -57,9 +43,8 @@ function TopHeader(props) {
                                     <ul className="top-social-media pull-right">
                                         <Link to={"/user"}>
                                             <img className="avatar-custom"
-                                                 src={avatar}
+                                                 src={user.avatar}
                                                  alt="avatar">
-
                                             </img></Link>
                                         <li>
                                             <a onClick={handleLogout} className="sign-in"><i
