104880481b980cb577f8af4436c569151ee7a032
HoangTran2108 <119851922+HoangTran2108@users.noreply.github.com>
2023-06-05 08:31:11 +0700

    .

--- src/components/BookingCard.jsx
+++ src/components/BookingCard.jsx
@@ -18,8 +18,7 @@ function BookingCard(props) {
     const [isValid, setValid] = useState();
     const {id} = useParams();
     const [transferDate, setTransferDate] = useState('')
-<<<<<<< HEAD
-    const homeStatus = props.homeStatus;
+
     const [room, setRoom] = useState('1');
     const currentDate=new Date();
     const [customerId, setCustomerId] = useState("");
@@ -29,8 +28,6 @@ function BookingCard(props) {
         uId:user.id,
         time:currentDate
     });
-=======
->>>>>>> f51eb99c01972dbbac2cff941aa8f9c5b3e57690
 
     const Send=()=>{
             socket.emit("send_message", { message, room });
@@ -40,12 +37,7 @@ function BookingCard(props) {
         event.preventDefault();
         setButtonOpen(true)
         setButtonClose(false)
-<<<<<<< HEAD
-    }
-    console.log(user)
-=======
     };
->>>>>>> f51eb99c01972dbbac2cff941aa8f9c5b3e57690
     const buttonCloseHandler = (event) => {
         event.preventDefault();
         setButtonClose(false);
@@ -199,7 +191,6 @@ function BookingCard(props) {
                     confirmButtonText: 'OK'
                 });
             });
-<<<<<<< HEAD
         axios.put(`http://localhost:8080/homes/after-booking/${id}`, data.homes.id, {
             headers: {
                 'Content-Type': 'application/json'
@@ -213,8 +204,6 @@ function BookingCard(props) {
             }).catch((err) => {
             console.error(err.message)
         })
-=======
->>>>>>> f51eb99c01972dbbac2cff941aa8f9c5b3e57690
     }
 
 }
